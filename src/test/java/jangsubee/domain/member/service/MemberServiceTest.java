7e956afae72bd49250214204e46914ff071798cc
yeon127 <lovegroup@naver.com>
2021-04-17 11:28:59 +0900

    add test

---
+++ src/test/java/jangsubee/domain/member/service/MemberServiceTest.java
@@ -0,0 +1,33 @@
+package jangsubee.domain.member.service;
+
+import jangsubee.domain.member.entity.Member;
+import jangsubee.domain.member.entity.enums.MemberTypeEnum;
+import jangsubee.domain.member.service.dto.MemberJoinIn;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    @DisplayName("멤버등록 테스트")
+    void register() {
+        // given
+        MemberJoinIn memberJoinIn = new MemberJoinIn("STAFF21", "양꼬치", MemberTypeEnum.TEAM_MEMBER, "20210417", "99991231", "1234");
+
+        // when
+        String createdMemberId = memberService.join(memberJoinIn);
+        Member member = memberService.findMember(createdMemberId);
+
+        // then
+        Assertions.assertThat(member.getId()).isEqualTo(memberJoinIn.getId());
+    }
+}
